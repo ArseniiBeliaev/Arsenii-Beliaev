db330dc2f3c016e97132e0c57024e5cb5cb3d06d
beliaev.ai@phystech.edu <beliaev.ai@phystech.edu>
2021-04-19 14:48:40 +0300

    v1.0

---
+++ 2-6-F.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+ 
+int main() {
+    int size1;
+    int size2;
+    std::cin >> size1;
+    int array1[size1+1];
+    for (int i = 1; i < size1 + 1; ++i) {
+        std::cin >> array1[i];
+    }
+ 
+    std::cin >> size2;
+    int array2[size2+1];
+    for (int i = 1; i < size2 + 1; ++i) {
+        std::cin >> array2[i];
+    }
+ 
+ 
+    int array_ans[size1 + 1][size2 + 1];
+    for (int i = 0; i < size1 + 1; ++i) {
+        for (int j = 0; j < size2 + 1; ++j) {
+            array_ans[i][j] = 0;
+        }
+    }
+ 
+   for (int i = 1; i < size1 + 1; ++i) {
+       for (int j = 1; j < size2 + 1; ++j) {
+           if (array1[i] != array2[j]) {
+               array_ans[i][j] = std::max(array_ans[i][j-1],array_ans[i-1][j]);
+           }
+           else {
+               array_ans[i][j] = array_ans[i-1][j-1]+1;
+           }
+       }
+   }
+ 
+ 
+ 
+    std::cout << array_ans[size1][size2] << '\n';
+    return 0;
+}
