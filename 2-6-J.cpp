a716cde14d5bd4fe824cf36d9f77a1901951b347
beliaev.ai@phystech.edu <beliaev.ai@phystech.edu>
2021-04-19 14:50:55 +0300

    v1.0

---
+++ 2-6-J.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+int main() {
+    long long bag;
+    long long num;
+    std::cin >> bag >> num;
+    long long items[num+1];
+    items[0] = 0;
+    for (int i = 1; i < num + 1; ++i) {
+        std::cin >> items[i];
+    }
+    long long dp[num+1][bag+1];
+    for (int i = 0; i < num + 1 ; ++i) {
+        for (int j = 0; j < bag + 1; ++j) {
+            dp[i][j] = 0;
+        }
+    }
+    for (int i = 1; i < num + 1; ++i) {
+        for (int j = 0; j < bag + 1; ++j) {
+            dp[i][j] = std::max(dp[i-1][j],dp[i][j]);
+            if (items[i] + j < bag + 1) {
+                dp[i][j+items[i]] = std::max(dp[i][j+items[i]],dp[i-1][j] + items[i]);
+            }
+        }
+    }
+
+    long long max = 0;
+    for (int i = 1; i < num + 1; ++i) {
+        for (int j = 1; j < bag + 1; ++j) {
+            if (max < dp[i][j]) {
+                max = dp[i][j];
+            }
+        }
+    }
+    std::cout << max;
+
+    return 0;
+}
