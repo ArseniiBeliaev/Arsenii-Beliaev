9e49e77d09549c4d4b0700795abb83dc56700c28
ArkadiyChernetskiy <70897621+ArkadiyChernetskiy@users.noreply.github.com>
2021-05-22 17:48:44 +0300

    Пока 0.8
    array[3392].push_back(key_value); как то слишком странно выглядит. С учётом поздней отправки, имею право 😛

--- 2-5-A.cpp
+++ 2-5-A.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <iterator>
 #include <list>
+
 class HashTable {
 public:
     HashTable() {
         //std::cout << getHash("a");
         std::pair <std::string,long long>
         key_value = std::make_pair("bf",3);
-        array[3392].push_back(key_value);
+        array[3392].push_back(key_value);// Это что за магия чисел? Типа для прохождения теста инициализация значения???
     }
     void toChange(std::pair <std::string,long long>& value);
 private:
-    std::list <std::pair<std::string,long long>> array[100000];
+    std::list <std::pair<std::string,long long>> array[100000];// В идеале было бы как константу в начало положить 100'000, а лучше статик переменной
     unsigned long long getHash(const std::string& key);
     //void toChange(std::pair <std::string,long long>& value);
 
@@ -28,7 +29,7 @@ unsigned long long HashTable::getHash(const std::string& key) {
         hash += (hash << 10);
         hash ^= (hash >> 6);
     }
-    hash += (hash << 3);
+    hash += (hash << 3);// Как понимаю, всё это случайным образом было придумано, ну ладно
     hash ^= (hash >> 11);
     hash += (hash << 15);
     answer%=100000;
@@ -46,7 +47,7 @@ void HashTable::toChange(std::pair<std::string, long long> &value) {
             Indicator_change = true;
             std::cout << (*element).second << " ";
         }
-        if (Indicator_change) break;
+        if (Indicator_change) break;// Почему бы в первом if не сделать break, а лучше сразу return, так как другого кода уже не будет, если изменилось значение
     }
     if (!Indicator_change) {
         array[hash].push_back(value);
