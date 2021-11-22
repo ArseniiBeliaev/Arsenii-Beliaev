f9fad3dfa6ec69037d4ce757be14a6c41b6436d8
Арсений <arsenij@MacBook-Pro-Arsenij.local>
2021-11-22 16:26:16 +0300

    v1.0

---
+++ 3-11-D.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+#include <functional>
+#include <array>
+
+
+
+int main() {
+    std::string str;
+    std::vector <std::string> words; 
+    std::vector <std::string> points; 
+    bool first_point = false;
+    std::string point;
+    std::string word;
+    std::cin >> str;
+    if (str[0] == '.') {
+        first_point = true;
+    }
+    for (int i = 0; i < str.size();) {
+        while (i < str.size() && str[i] == '.')
+        {
+            point += '.';
+            ++i;
+        }
+        if (point.size()) {
+            points.push_back(point);
+        }
+        point.clear();
+        while ( i < str.size() && str[i] != '.')
+        {
+            word += str[i];
+            ++i;
+        }
+        if (word.size()) {
+            words.push_back(word);
+        }
+        word.clear();
+    }  
+
+    std::sort(words.begin(),words.end());
+    int i = 0;
+    int j = 0; 
+    while (i != words.size() || j != points.size()) {
+        if (first_point) {
+            std::cout << points[j];
+            first_point = false;
+            ++j;
+        }
+        if (i < words.size()) {
+            std::cout << words[i];
+            ++i;
+        }
+        if (j < points.size()) {
+            std::cout << points[j];
+            ++j;
+        }
+    }
+    return 0;   
+}
